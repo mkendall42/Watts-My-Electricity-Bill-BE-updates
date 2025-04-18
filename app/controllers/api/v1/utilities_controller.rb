a907760415db207b8f062f376eea6e600434df73
Mark Kendall <mkendall.tutor@gmail.com>
2025-04-18 15:39:41 -0500

    Add UtilitiesController#validate_params existence check, update test

--- app/controllers/api/v1/utilities_controller.rb
+++ app/controllers/api/v1/utilities_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UtilitiesController < ApplicationController
 
   def index
     #Check request and params from FE, verify presence and validity
-    if !validate_params(params)
+    if validate_params(params) != []
       render json: { message: "Error" }, status: 400
       # return
     else
@@ -26,6 +26,19 @@ class Api::V1::UtilitiesController < ApplicationController
     #This will have more details later.  Plan is to return a hash with any errors (or empty hash if successful)
     #Later later: might have it raise exceptions one by one
 
-    return false
+    #Returns empty array if ok (params good), nonempty array if error(s)
+
+    # binding.pry
+
+    messages = []
+    required_params = [:nickname, :latitude, :longitude, :residence_type, :num_residents, :efficiency_level, :username]
+    required_params.each do |required_param|
+      messages << "Error: required parameter #{required_param} is missing." if !params[required_param].present?
+    end
+
+    #Also check data ranges are valid (could move to Rails 'validate' if we add this to DB later)
+    # check_lat_long(params[:latitude], params[:longitude])
+
+    return messages
   end
 end
