fe77f6d5f0ef6dd981c26332d430cb5c92bc8af5
Jackson Bickler <jbickler4@gmail.com>
2025-04-22 20:00:44 -0600

    fixed gateway

--- app/gateways/eia_gateway.rb
+++ app/gateways/eia_gateway.rb
@@ -1,29 +1,15 @@
 class EiaGateway
-  BASE_PATH = '/electricity/retail-sales/data/'
-
   def report_details
-    get_url(BASE_PATH, {
-      api_key: ENV['eia_api_key'],
-      frequency: 'monthly',
-      'data[0]': 'price',
-      'sort[0][column]': 'period',
-      'sort[0][direction]': 'desc',
-      offset: '0',
-      length: '5000'
-    })
-  end
-
-  def get_url(path, params = {})
-    response = conn.get(path) do |req|
-      req.params.update(params)
+    response = Faraday.get("https://api.eia.gov/v2/electricity/retail-sales/data/") do |req|
+      req.params['api_key'] = ENV['eia_api_key']
+      req.params['frequency'] = 'monthly'
+      req.params['data[0]'] = 'price'
+      req.params['sort[0][column]'] = 'period'
+      req.params['sort[0][direction]'] = 'desc'
+      req.params['offset'] = '0'
+      req.params['length'] = '5000'
     end
 
     JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def conn
-    Faraday.new(url: 'https://api.eia.gov/v2') do |faraday|
-      faraday.headers['Content-Type'] = 'application/json'
-    end
   end
 end
