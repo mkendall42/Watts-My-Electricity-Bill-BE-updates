3035a8d85b7b3e01c44386ec568052f5094dbec7
Jackson Bickler <jbickler4@gmail.com>
2025-04-22 18:35:19 -0600

    gateway_done

--- app/gateways/eia_gateway.rb
+++ app/gateways/eia_gateway.rb
@@ -1,9 +1,29 @@
 class EiaGateway
+  BASE_PATH = '/electricity/retail-sales/data/'
 
-  
+  def report_details
+    get_url(BASE_PATH, {
+      api_key: ENV['eia_api_key'],
+      frequency: 'monthly',
+      'data[0]': 'price',
+      'sort[0][column]': 'period',
+      'sort[0][direction]': 'desc',
+      offset: '0',
+      length: '5000'
+    })
+  end
+
+  def get_url(path, params = {})
+    response = conn.get(path) do |req|
+      req.params.update(params)
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def conn
     Faraday.new(url: 'https://api.eia.gov/v2') do |faraday|
-      faraday.headers['Authorization'] = "Bearer #{ENV['eia_token']}"
+      faraday.headers['Content-Type'] = 'application/json'
     end
   end
 end
