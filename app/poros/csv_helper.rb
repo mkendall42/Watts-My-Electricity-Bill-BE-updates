74f0cd6449fce4af3be7ad7ea55b2ccb5d2904be
little-Patrick <Little.patrick@outlook.com>
2025-04-23 17:36:40 -0600

    Fix: Variable and Class names

--- app/poros/csv_helper.rb
+++ app/poros/csv_helper.rb
@@ -1,14 +1,15 @@
 require 'csv'
 
-class CSVHelper
+class CsvHelper
   attr_reader :data
 
+  @data = []
+
   def self.utilityCSV(csv_data)
-    @data = []
     CSV.foreach(csv_data, headers: true, header_converters: :symbol) do |row|
-      data << CSV_Utility.new(
-        row[:zip].to_i
-        row[:state]
+      @data << CsvUtility.new(
+        row[:zip].to_i,
+        row[:state],
         row[:res_rate].to_f
       )
     end
@@ -16,13 +17,21 @@ class CSVHelper
   end
 
   def self.price_by_zip(zipcode)
-    zip_data = @data.select { |area| area.zip == zipcode }
-    return zip_data.res_rate
+    zip_data = @data.select { |area| area.zipcode == zipcode.to_i }
+    zip_data[0].state
+    return zip_data[0].res_rate
+  end
+
+  def self.state_by_zip(zipcode)
+    zip_data = @data.select { |area| area.zipcode == zipcode }
+    zip_data[0].state
   end
 
   def self.price_by_state(state)
+    binding.pry
     state_data = @data.select { |area| area.state == state }
     state_total = state_data.sum { |area| area.res_rate }
+    binding.pry
     return state_total / state_data.length
   end
 
