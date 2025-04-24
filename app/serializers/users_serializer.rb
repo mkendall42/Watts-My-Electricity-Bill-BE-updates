fd13ac7e8db57ac73a5df0f2509f9d0467198110
little-Patrick <Little.patrick@outlook.com>
2025-04-23 19:47:12 -0600

    Feat: index for getting all users w/ serializer, route, controller, and seed data

--- app/serializers/users_serializer.rb
+++ app/serializers/users_serializer.rb
@@ -1,4 +1,13 @@
 class UsersSerializer
+  def self.format_users(users)
+    users.map do |user|
+      {
+        id: user.id,
+        username: user.username
+      }
+    end
+  end
+
   def self.format_single_user(user)
     report_list = user.reports.map do |report|
       {
