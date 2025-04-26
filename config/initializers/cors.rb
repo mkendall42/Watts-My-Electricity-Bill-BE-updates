0c19c7e546c98f39bf9cf156f730d544aa1c1050
little-Patrick <Little.patrick@outlook.com>
2025-04-26 07:50:51 -0600

    Fix: cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://watts-my-electricity-bill-fe.onrender.com/', 'http://localhost:5173' # Your Vite dev server
+    # origins 'https://watts-my-electricity-bill-fe.onrender.com/', 'http://localhost:5173' # Your Vite dev server
     
 
     resource '*',
