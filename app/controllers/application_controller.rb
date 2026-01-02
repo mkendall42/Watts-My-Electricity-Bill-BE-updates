f51bd71917ae2233552cfdb16e967949202f4bef
Mark Kendall <mkendall.tutor@gmail.com>
2026-01-02 16:55:05 -0600

    Fix: exception rescuing for puma database hang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  #These make the most sense in the parent class, given their frequent usage
+  #NOTE: these exceptions are captured in middleware BEFORE here; for now, this doesn't do anything (even if it should)
+  rescue_from ActiveRecord::NoDatabaseError, ActiveRecord::ConnectionNotEstablished, with: :handle_database_errors
+
+  protected
+  
+  def handle_database_errors(exception)
+    render json: { status: :service_unavailable, message: "Database connection error: #{exception.error_message}" }, status: :service_unavailable
+  end
 end
